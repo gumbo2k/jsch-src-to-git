dc5adf9bb21480a53b4485c3ff76c2cd65ef6e67
gumbo2k <gumbo2k@users.noreply.github.com>
2017-09-26 12:52:13 +0200

    simple download shell script
    Downloading from sourceforge with a browser is painful. 
    There is the forced delay, then the files don't get the right content type, 
    so you can't say "Save file" and "Always do this for this kind of file".
    Fortunately wget understands redirects AND is preserves the file's mtime!

---
+++ dl-from-sf.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+for x in $(seq 3 13) ; do 
+    wget -O jsch-test-0.0.${x} https://sourceforge.net/projects/jsch/files/jsch/0.0.${x}/jsch-0.0.${x}.zip/download 
+done
+
+for x in $(seq 0 54) ; do
+    wget -O jsch-test-0.1.${x} https://sourceforge.net/projects/jsch/files/jsch/0.1.${x}/jsch-0.1.${x}.zip/download 
+done
+
